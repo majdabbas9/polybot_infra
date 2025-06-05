7d49abcc0bba3bcef316493e7c015d4064b1f765
majdabbas <majd.abbas999@gmail.com>
2025-06-05 18:03:48 +0300

    setting up infra with docker

--- Nginx/deploy.sh
+++ Nginx/deploy.sh
@@ -54,7 +54,6 @@ sudo docker stop mynginx || true
 sudo docker rm mynginx || true
 
 sudo docker run -d --name mynginx \
-  --restart unless-stopped \
   -p 443:443 \
   -v "$NGINX_CONF_DST:/etc/nginx/conf.d" \
   -v "$CERTS_DIR:/etc/nginx/ssl" \
