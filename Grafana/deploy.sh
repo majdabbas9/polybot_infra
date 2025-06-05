934b7e6c771d0ac95f3fc7fee5310b12c80517d9
majdabbas <majd.abbas999@gmail.com>
2025-06-06 00:03:20 +0300

    setting up infra with docker

---
+++ Grafana/deploy.sh
@@ -0,0 +1,15 @@
+sudo docker pull grafana/grafana
+
+# Stop and remove existing container
+echo "🧹 Checking for old Grafana container..."
+
+if sudo docker ps -a --format '{{.Names}}' | grep -q '^mygrafana'; then
+  echo "🧹 Removing old container 'mygrafana'..."
+  sudo docker rm -f mygrafana
+else
+  echo "✅ No existing container named 'mygrafana' found."
+fi
+
+sudo docker run \
+  --name=mygrafana -d \
+  -p 3000:3000 grafana/grafana
