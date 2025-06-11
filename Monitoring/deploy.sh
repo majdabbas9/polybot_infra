eb96d1fbd2f4bd0bf2056213adcf4929b7ea72c7
majdabbas <majd.abbas999@gmail.com>
2025-06-11 14:40:30 +0300

    setting up infra with docker

--- Monitoring/deploy.sh
+++ Monitoring/deploy.sh
@@ -9,8 +9,8 @@ cat > "$ENV_FILE" <<EOF
 PROMETHEUS_CONFIG=$WORKing_DIR/prometheus.yml
 EOF
 
-sudo docker compose -f docker-compose.monitoring.yaml down
-sudo docker compose -f docker-compose.monitoring.yaml up -d
+sudo docker compose -f $HOME/docker-compose.monitoring.yaml down
+sudo docker compose -f $HOME/docker-compose.monitoring.yaml up -d
 
 echo "✅ Prometheus is running at http://localhost:9090"
 echo "✅ Grafana is running at http://localhost:3000"
