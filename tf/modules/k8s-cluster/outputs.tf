d3ce7c4f9b5b037ebb23871958dd71380c71b39b
majdabbas <majd.abbas999@gmail.com>
2025-07-05 18:26:44 +0300

    k8s setup

--- tf/modules/k8s-cluster/outputs.tf
+++ tf/modules/k8s-cluster/outputs.tf
@@ -2,3 +2,18 @@ output "control_plane_public_ip" {
   value = aws_instance.k8s_cp.public_ip
   description = "Public IP of the control plane node"
 }
+output "dev_s3_bucket_id" {
+  value = aws_s3_bucket.bucket_dev.id
+}
+
+output "prod_s3_bucket_id" {
+  value = aws_s3_bucket.bucket_prod.id
+}
+
+output "dev_sqs_queue_url" {
+  value = aws_sqs_queue.sqs_dev.id
+}
+
+output "prod_sqs_queue_url" {
+  value = aws_sqs_queue.sqs_prod.id
+}
